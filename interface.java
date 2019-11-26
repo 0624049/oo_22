1910e4085aa4f8c2870c472700d25618b74f1561
0624049 <55127460+0624049@users.noreply.github.com>
2019-11-26 16:21:22 +0800

    Update interface.java

--- interface.java
+++ interface.java
@@ -1,12 +1,13 @@
 interface Shape{
-    abstract void getArea(double area);
+    abstract double getArea();
 }
 class Rectangle implements Shape{
 
     double length,width;
 
-    public void getArea(double length,double width){
-        Sytem.out.println("getArea="+length*width);
+    public double getArea(){
+        double area=length*width;
+        return area;
     }
     public void toString(String toStr){
 
@@ -15,21 +16,20 @@ class Rectangle implements Shape{
 class Triangle implements Shape{
     double base,height;
 
-    public void getArea(double base,double height){
-        Sytem.out.println("getArea="+(base*height)/2);
+    public double getArea(){
+        double area=(base*height)/2;
+        return area;
     }
     public void toString(String toStr){
 
     }
 }
-public class interface{
+public class Main{
     public static void main(String arg[]){
         Rectangle rec;
-        rec=new Rectangle(5,9);
-        rec.getArea();
+        rec.getArea(5,9);
 
         Triangle tri;
-        tri=new Triangle(7,8);
-        tri.getArea();
+        tri.getArea(7,8);
     }
 }
